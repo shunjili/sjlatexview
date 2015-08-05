0d6193109cca7f089afc813a42753c38855fbb15
Shunji Li <shunjil@twitter.com>
2015-08-05 11:16:48 -0700

    rename

--- LatexView/ViewController.swift
+++ LatexView/ViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var latexWebView: SJLatexView!
+    @IBOutlet weak var latexView: SJLatexView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        latexWebView.layer.borderColor = UIColor.grayColor().CGColor
-        latexWebView.layer.borderWidth = 2.0
+        latexView.layer.borderColor = UIColor.grayColor().CGColor
+        latexView.layer.borderWidth = 2.0
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         let latexString = "When $a \\ne 0$, there are two solutions to $ax^2 + bx + c = 0$ and they are$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$"
-        latexWebView.loadLatexString(latexString)
+        latexView.loadLatexString(latexString)
     }
 }
 
